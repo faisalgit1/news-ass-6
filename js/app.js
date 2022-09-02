293783fbb14137b76ab454c12558f2a5875bad7d
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-09-03 00:36:50 +0600

    load-news

--- js/app.js
+++ js/app.js
@@ -18,3 +18,11 @@ const displayNewsCategory = async (categories) => {
 
     })
 }
+const loadNews = async () => {
+    const url = ` https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.data)
+}
+
+loadNews()
