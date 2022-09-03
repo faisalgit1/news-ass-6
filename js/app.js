40a0bf649a84df6cdfa28f27d5f5bcf8ab8ba06a
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-09-03 13:41:25 +0600

    display-load-news-text

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 const loadNewsCategory = async () => {
-    const url = "https://openapi.programming-hero.com/api/news/categories"
+    const url = ` https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url)
     const data = await res.json()
     displayNewsCategory(data.data.news_category)
+    return data;
 }
 loadNewsCategory()
 
@@ -12,17 +13,36 @@ const displayNewsCategory = async (categories) => {
         // console.log(category)
         const li = document.createElement('li');
         li.innerHTML = `
-        <a onclick =loadNews()>${category.category_name}</a>
+        <a onclick ="loadNews('${category.category_id}')">${category.category_name}</a>
         `;
         showCategory.appendChild(li);
 
     })
 }
-const loadNews = async () => {
-    const url = ` https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`
+const loadNews = async (category_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data.data)
+    displayLoadNews(data.data)
 }
 
 loadNews()
+const displayLoadNews = async (newses) => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    newses.forEach(news => {
+        const { title, thumbnail_url, details, author, total_view } = news;
+        const { name, published_date, img } = author;
+        console.log(news)
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img class="h-80 w-70" src="${thumbnail_url}" alt="Album"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">${title}</h2>
+                    <P class="text-current">${details.length > 200 ? details.slice(0, 200) + '...' : details} <P>
+                   
+            `;
+        newsContainer.appendChild(div);
+    })
+}
