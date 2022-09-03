de592fe34b595875f2e4d5974939246ef963c1fd
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-09-03 22:45:33 +0600

    update

--- js/app.js
+++ js/app.js
@@ -10,30 +10,38 @@ const loadNewsCategory = async () => {
 loadNewsCategory()
 
 const displayNewsCategory = async (categories) => {
+    loading(true)
     const showCategory = document.getElementById('categories-list');
     categories.forEach(category => {
         // console.log(category)
-        const li = document.createElement('li');
-        li.classList.add('font-medium')
-        li.innerHTML = `
-        <a onclick ="loadNews('${category.category_id}')">${category.category_name}</a>
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button class="bg-primary text-white border border-0 rounded fs-4 " onclick ="loadNews('${category.category_id}')">${category.category_name}</button >
         `;
-        showCategory.appendChild(li);
+        showCategory.appendChild(div);
 
     })
+}
+const loading = isloading => {
+    const spinner = document.getElementById('spinner')
+    if (isloading === true) {
+        spinner.classList.remove('d-none')
+    }
+    else {
+        spinner.classList.add('d-none')
+    }
 }
 const loadNews = async (category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url)
     const data = await res.json()
     displayLoadNews(data.data)
+    loading(false)
 }
 
 loadNews()
 const displayLoadNews = async (newses) => {
-    if (newses.length === 0) {
-        alert('no data');
-    }
+
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent = '';
     newses.forEach(news => {
@@ -42,48 +50,52 @@ const displayLoadNews = async (newses) => {
         const { number } = rating;
         console.log(news)
         const div = document.createElement('div');
+        div.classList.add('col')
         div.innerHTML = `
-            <div class="card lg:card-side bg-base-100 shadow-xl">
-                <figure><img class="h-80 w-70" src="${thumbnail_url ? thumbnail_url : 'not found'}" alt="Album"></figure>
-                <div class="card-body">
-                    <h2 class="card-title text-2xl font-extrabold">${title ? title : 'not found'}</h2>
-                    <P class="font-semibold">${details.length > 200 ? details.slice(0, 200) + '...' : details} <P>
-                    <div class="stats ">
-                    <div class="stat flex w-30">
-                        <div class="avatar">
-                            <div class="w-16 rounded-full">
-                                <img src="${img ? img : 'not found'}" />
-                            </div>
-                        </div>
-                        <div>
-                            <h1 class="stat-value">${name ? name : 'not found'}</h1>
-                            <h2 class="font-semibold">${published_date ? published_date : 'not found'}</h2>
-                        </div>
-                    </div>
-                    <div class="stat flex">
-                        <div>
-                            <img src="images/images.jpg" alt="">
-                        </div>
-                        <div class="stat-value text-secondary">${total_view ? total_view : 'not found'}</div>
-                    </div>
+        <div class="card">
+            <img  src="${thumbnail_url ? thumbnail_url : 'not found'}"  class="card-img-top img-fluid" alt="..." >
+            <div class="card-body">
+                <h5 class="card-title">${title ? title : 'not found'}</h5>
+                <p class="card-text">${details.slice(0, 200)}...</p>
+                <div class="d-flex align-items-center justify-content-between">
+                <div class="d-flex">
+                   <img src="${img ? img : 'not found'}" width="40">
+                    <p class="ps-2 fw-semibold">${name ? name : 'not found'} </p>
                 </div>
-                <div class="stat">
-                <label for="my-modal-3" onclick="showModal('${image_url}', '${title}', '${number}', '${details}')" class="btn btn-primary w-20 modal-button">Details</label>
                 <div>
-                
+                <p><span class="mx-2 eye-icon"><i class="fa-regular fa-eye"></i></span>${total_view ? total_view : 'not found'}</p>
                 </div>
+                <div>
+                <button type="button" onclick="loadIdNews('${news._id}')"   class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+                </div>
+                
             </div>
-            `;
+        </div>
+        `
         newsContainer.appendChild(div);
     })
 }
 
-const showModal = async (image_url, title, number, details) => {
-    const modalBody = document.getElementById('modal-body');
-    modalBody.innerHTML = `
-    <img class="h-80 w-70" src="${image_url ? image_url : 'not found'}" alt="Album">
-    <h2 class="card-title text-2xl font-extrabold">${title ? title : 'not found'}</h2>
-    <h2 class="text-2xl font-semibold">Ratings:${number}</h2>
-    <P class="font-semibold">${details} <P>
-`;
+const loadIdNews = (modal) => {
+    const url = `https://openapi.programming-hero.com/api/news/${modal}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayModalData(data.data[0]))
+        .catch(error => console.log(error))
+}
+
+// Display news in Modal 
+const displayModalData = (news) => {
+    const bodyModal = document.getElementById('body-modal');
+    bodyModal.innerHTML = `
+    <h2>${news.title}</h2>
+    <img class="img-fluid" src="${news.image_url}">
+    <hr>
+    <h2>Ratings: ${news.rating.number}</h2>
+    <hr>
+    <P>${news.details}</P>
+    
+    `
 }
